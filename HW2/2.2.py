b74de9e5e986755f3e0337bec0ab00566288faab
MadRock33 <70816297+MadRock33@users.noreply.github.com>
2024-01-20 16:22:37 +0300

    new hw

---
+++ HW2/2.2.py
@@ -0,0 +1,11 @@
+def flip_string(input_str:str) -> str:
+    string_words = input_str.split()
+    result = []
+    for word in reversed(string_words):
+        result.append(word)
+    return ' '.join(result).capitalize()
+
+if __name__ == '__main__':
+    input_str = input("Input a string: ")
+    output_str = flip_string(input_str)
+    print("Flip string:", output_str)
